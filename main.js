056e27367a75afa89cf8adb211716cbcec08274e
Yerlis09 <castellaryerlis18@gmail.com>
2023-08-14 23:07:34 -0500

    creacrion de la actividad 1 de js, lista de contacto 1

--- main.js
+++ main.js
@@ -1,23 +1,72 @@
-import {guardarContacto, cargarContactos} from './js/funciones.js'
-
-const nombre = document.querySelector('.nombre')
-const numero = document.querySelector('.numero')
-const direccion = document.querySelector('.direccion')
-const btnAddContact = document.querySelector('.btn-add-contact')
-
-const contactList = document.querySelector('.contact-list')
-
-const db = window.localStorage
-
-btnAddContact.onclick = () => {
-    let contacto = {
-        id: Math.random(1, 100),
-        nombre: nombre.value,
-        numero: numero.value,
-        direccion: direccion.value,
+// defino mi lista de contactos
+let listaDeContactos = [
+    primerContacto = {
+        nombre: 'Yerlys',
+        telefono: 300584990,
+        direccion: 'barrio nariño'
     }
-    guardarContacto(db, contacto)
+
+]
+console.log(listaDeContactos);
+
+// funcion que permite crear un nuevo contacto
+function agregarContacto(segundoContacto) {
+    for (let i = 0; i < listaDeContactos.length; i++) {
+        if (segundoContacto.telefono != listaDeContactos[i].telefono) {
+            listaDeContactos.push(segundoContacto);
+        } else { console.log("el contacto ya existe, ingrese uno nuevo") }
+    }
+};
+
+// funcion que permite mostrar por consola toda nuestra lista de contactps
+function imprimirContactos() {
+    for (var i = 0; i < listaDeContactos.length; i++) {
+        console.log(listaDeContactos[i]);
+    }
+};
+// funcion para actualizar un contacto existente en la lista
+function actualizarContacto(indice, nuevoNombre) {
+    if (indice !== -1) {
+        listaDeContactos[indice].nombre = nuevoNombre;
+    }
+};
+
+function mostrarUnContacto(telefono) {
+    for (let i = 0; i < listaDeContactos.length; i++) {
+        if (listaDeContactos[i].telefono == telefono) {
+            console.log(listaDeContactos[i])
+        }
+    }
 }
 
-cargarContactos(db, contactList)
+// Función para borrar un contacto existente de la lista
+function borrarContacto(telefono) {
+    for (let i = 0; i < listaDeContactos.length; i++) {
+        if (listaDeContactos[i].telefono == telefono) {
+            listaDeContactos.splice(i, 1);
+        }
+    }
+};
 
+const segundoContacto = new Object()
+segundoContacto.nombre = 'Maria'
+segundoContacto.telefono = 3204857664
+segundoContacto.direccion = 'barrio nariño'
+console.log("\nAgregando segundo contacto");
+agregarContacto(segundoContacto);
+
+console.log("Lista de contactos:");
+imprimirContactos();
+
+console.log("Contacto actualizado")
+actualizarContacto(1, 'PAOLA')
+console.log("Lista de contactos despues de actualizar:");
+imprimirContactos();
+
+console.log("mostrando un solo contacto")
+mostrarUnContacto(3204857664)
+
+console.log("\nBorrando contacto existente...");
+borrarContacto(3204857664);
+console.log("Contactos después de borrar un contacto:");
+imprimirContactos();
